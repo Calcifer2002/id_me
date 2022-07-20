41b6983905289539c57c8389d842c6074eaf04ae
Calcifer2002 <jeegi.shanker@gmail.com>
2022-07-20 20:16:32 +0530

    can upload images to firestore

--- lib/pages/collect_data.dart
+++ lib/pages/collect_data.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
@@ -15,109 +17,110 @@ class Collectdata extends StatefulWidget {
 }
 
 class _CollectdataState extends State<Collectdata> {
-File? imageFileFace;
-File? imageFileID;
+  File? imageFileFace;
+  File? imageFileID;
+  String folderFace = "Face";
+  String folderID = "ID";
+  FirebaseFirestore firestoreRef = FirebaseFirestore.instance;
+  FirebaseStorage firebaseStorageRef = FirebaseStorage.instance;
 
   //
 
-
-    //   });
-    // }
-
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child:SingleChildScrollView(
-
-            child: Column(children: [
-              SizedBox(height: 100,),
-
-              if(imageFileFace != null)
-                Container(
-                    width: 250,
-                    height: 300,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      image: DecorationImage(
-                          image: FileImage(imageFileFace!),
-                          fit: BoxFit.cover
-                      ),
-                      border: Border.all(width: 8, color: Colors.black),
-                      borderRadius: BorderRadius.circular(12.0),
-                    )
-
-
-
-                )else
-                Container(
-                  width: 200,
-                  height: 250,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    border: Border.all(width: 8, color: Colors.black12),
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-
-                ),TextButton(
-                onPressed: () async {
-                  getImageFace(source: ImageSource.camera);
-                },
-                child: Text("Upload Face", style: TextStyle(fontSize: 20)),
+            child: SingleChildScrollView(
+                child: Column(children: [
+          SizedBox(
+            height: 100,
+          ),
+          if (imageFileFace != null)
+            Container(
+                width: 250,
+                height: 300,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      image: FileImage(imageFileFace!), fit: BoxFit.cover),
+                  border: Border.all(width: 8, color: Colors.black),
+                  borderRadius: BorderRadius.circular(12.0),
+                ))
+          else
+            Container(
+              width: 200,
+              height: 250,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                border: Border.all(width: 8, color: Colors.black12),
+                borderRadius: BorderRadius.circular(12.0),
               ),
-              if(imageFileID != null)
-                Container(
-                    width: 250,
-                    height: 300,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      image: DecorationImage(
-                          image: FileImage(imageFileID!),
-                          fit: BoxFit.cover
-                      ),
-                      border: Border.all(width: 8, color: Colors.black),
-                      borderRadius: BorderRadius.circular(12.0),
-                    )
-
-
-
-                )else
-                Container(
-                  width: 200,
-                  height: 250,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    border: Border.all(width: 8, color: Colors.black12),
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-
-                ),TextButton(
-                onPressed: () async {
-                  getImageID(source: ImageSource.camera);
-                },
-                child: Text("Upload ID", style: TextStyle(fontSize: 20)),
-              ),SizedBox(height: 30,),TextButton(
-                onPressed: () async {
-                  getImageFace(source: ImageSource.camera);
-                },
-                child: Text("SEND TO VERIFY", style: TextStyle(fontSize: 20)),
+            ),
+          TextButton(
+            onPressed: () async {
+              getImageFace(source: ImageSource.camera);
+            },
+            child: Text("Upload Face", style: TextStyle(fontSize: 20)),
+          ),
+          if (imageFileID != null)
+            Container(
+                width: 250,
+                height: 300,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      image: FileImage(imageFileID!), fit: BoxFit.cover),
+                  border: Border.all(width: 8, color: Colors.black),
+                  borderRadius: BorderRadius.circular(12.0),
+                ))
+          else
+            Container(
+              width: 200,
+              height: 250,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                border: Border.all(width: 8, color: Colors.black12),
+                borderRadius: BorderRadius.circular(12.0),
               ),
-            ]))),
+            ),
+          TextButton(
+            onPressed: () async {
+              getImageID(source: ImageSource.camera);
+            },
+            child: Text("Upload ID", style: TextStyle(fontSize: 20)),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextButton(
+            onPressed: () async {
+              _uploadImageFace();
+              _uploadImageID();
+            },
+            child: Text("SEND TO VERIFY", style: TextStyle(fontSize: 20)),
+          ),
+        ]))),
         floatingActionButton: Padding(
             padding: const EdgeInsets.only(left: 30),
             child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
               FloatingActionButton(
-                onPressed: () async {getImageFace(source: ImageSource.camera);},
+                onPressed: () async {
+                  getImageFace(source: ImageSource.camera);
+                },
                 child: Icon(Icons.face_rounded),
               ),
               SizedBox(width: 10),
               FloatingActionButton(
-                onPressed: () async {getImageID(source: ImageSource.camera);},
+                onPressed: () async {
+                  getImageID(source: ImageSource.camera);
+                },
                 child: Icon(Icons.add_card),
               ),
               Expanded(child: Container()),
@@ -127,34 +130,71 @@ File? imageFileID;
               ),
             ])));
   }
-void getImageFace({required ImageSource source}) async {
 
-  final file = await ImagePicker().pickImage(
-      source: source,
-      maxWidth: 640,
-      maxHeight: 480,
-      imageQuality: 70 //0 - 100
-  );
+  void getImageFace({required ImageSource source}) async {
+    final file = await ImagePicker().pickImage(
+        source: source,
+        maxWidth: 640,
+        maxHeight: 480,
+        imageQuality: 70 //0 - 100
+        );
 
-  if(file?.path != null){
-    setState(() {
-      imageFileFace = File(file!.path);
+    if (file?.path != null) {
+      setState(() {
+        imageFileFace = File(file!.path);
+      });
+    }
+  }
+
+  _uploadImageFace() async {
+    var key = firestoreRef.collection(folderFace);
+    String uploadFaceName =
+        DateTime.now().millisecondsSinceEpoch.toString() + "jpg";
+    Reference reference =
+        firebaseStorageRef.ref().child(folderFace).child(uploadFaceName);
+    UploadTask uploadTask = reference.putFile(File(imageFileFace!.path));
+
+    uploadTask.snapshotEvents.listen((event) {
+      print(event.bytesTransferred.toString() +
+          " " +
+          event.totalBytes.toString());
+    });
+    await uploadTask.whenComplete(() async{
+      var uploadlink = await uploadTask.snapshot.ref.getDownloadURL();
+      print(uploadlink);
     });
   }
-}
-void getImageID({required ImageSource source}) async {
+  _uploadImageID() async {
+    var key = firestoreRef.collection(folderID);
+    String uploadFaceID =
+        DateTime.now().millisecondsSinceEpoch.toString() + "jpg";
+    Reference reference =
+    firebaseStorageRef.ref().child(folderID).child(uploadFaceID);
+    UploadTask uploadTask = reference.putFile(File(imageFileFace!.path));
 
-  final file = await ImagePicker().pickImage(
-      source: source,
-      maxWidth: 640,
-      maxHeight: 480,
-      imageQuality: 70 //0 - 100
-  );
-
-  if(file?.path != null){
-    setState(() {
-      imageFileID = File(file!.path);
+    uploadTask.snapshotEvents.listen((event) {
+      print(event.bytesTransferred.toString() +
+          " " +
+          event.totalBytes.toString());
+    });
+    await uploadTask.whenComplete(() async{
+      var uploadlink = await uploadTask.snapshot.ref.getDownloadURL();
+      print(uploadlink);
     });
   }
-}
+
+  void getImageID({required ImageSource source}) async {
+    final file = await ImagePicker().pickImage(
+        source: source,
+        maxWidth: 640,
+        maxHeight: 480,
+        imageQuality: 70 //0 - 100
+        );
+
+    if (file?.path != null) {
+      setState(() {
+        imageFileID = File(file!.path);
+      });
+    }
+  }
 }
